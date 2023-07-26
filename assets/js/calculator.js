32ce3309b9724ee60589831c551bfe28dbe3b7a6
Kaso <kaso@Kasos-Air.localdomain>
2023-07-25 18:50:22 -0700

    Adding more silliness

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -5,12 +5,16 @@ $(document).ready(function() {
     var viewWidth = $(window).width();
     var viewHeight = $(window).height();
     var top, left;
+    var heading = document.querySelector('#header');
     
     // Spawn calculator on button click
     $(document).on("click", ".button-row > button", function() {
         left = Math.random() * (viewWidth-0);
         top = Math.random() * (viewHeight-0);
+        // new calculator placed randomly
         $calc(top,left);
+        // change heading
+        $compute(heading);
     });
   });
   
@@ -33,4 +37,21 @@ $(document).ready(function() {
         "margin-left":left,
         "float":divCount+1
     });
+
+    // Change heading
+    $compute = function(heading) {
+        var phrases = [
+            "Does not compute.",
+            "Are you serious?",
+            "I didn't feel anything.",
+            "What are you trying to do?",
+            "This is not a tip calculator.",
+            "It's a secret.",
+            "3.141592653589793238462643383279502884197",
+            "Sorry, that calculator is broken. Here's a new one.",
+            "I don't understand.",
+            "Error.",
+        ];
+        heading.textContent = phrases[Math.floor(Math.random() * 11)];
+    };
   };
